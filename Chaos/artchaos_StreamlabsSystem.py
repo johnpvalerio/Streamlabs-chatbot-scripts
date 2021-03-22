a53af21f01b488d18bc61a7eb26902dba211b2bf
John Paulo Valerio <johnp.valerio@gmail.com>
2021-03-21 22:42:51 -0400

    Added Twitch channel points functionality

--- Chaos/artchaos_StreamlabsSystem.py
+++ Chaos/artchaos_StreamlabsSystem.py
@@ -21,13 +21,13 @@ ScriptName = "Chaos art hotkeys Script"
 Website = "https://github.com/johnpvalerio"
 Description = "Dumb hotkey script for Streamlabs Bot"
 Creator = "AegisBlue"
-Version = "1.0.0"
+Version = "1.1.0"
 # ---------------------------------------
 # Variables
 # ---------------------------------------
 configFile = "config.json"
 settings = {}
-
+EventReceiver = None
 
 def Init():
     """
@@ -42,12 +42,7 @@ def Init():
             settings = json.load(file, encoding='utf-8-sig')
     except IOError:
         settings = {
-            "command": "!chaos",
-            "cost": 1000,
-            "useCost": True,
-            "userCd": 15,
-            "globalCd": 10,
-            "liveOnly": True,
+            "rewardName": "chaos",
             "programName": "",
             "outputMsg": "$user has chosen $action",
             "debugMode": False
@@ -56,58 +51,9 @@ def Init():
 
 def Execute(data):
     """
-    Processes/validates data to execute gambling game
     :param data: Data - chat message object
     :return: None
     """
-    if data.IsChatMessage() and (
-            data.GetParam(0).lower() == settings["command"] ) and (
-            (settings["liveOnly"] and Parent.IsLive()) or (not settings["liveOnly"])):
-        # check user
-        userID = data.User
-        userName = data.UserName
-        # user cooldown
-        if Parent.IsOnUserCooldown(ScriptName,settings['command'],data.User):
-            if settings["debugMode"]:
-                Parent.Log(ScriptName, userName + ' user cooldown: ' + str(Parent.GetUserCooldownDuration(ScriptName,settings['command'],userID)))
-            return
-        # global cooldown
-        if Parent.IsOnCooldown(ScriptName,settings['command']):
-            if settings["debugMode"]:
-                Parent.Log(ScriptName, 'global cooldown: ' + str(Parent.GetCooldownDuration(ScriptName,settings['command'])) )
-            return
-
-        # check points
-        userPoints = Parent.GetPoints(userID)
-        cost = settings['cost']
-        if cost > userPoints:
-            if settings["debugMode"]:
-                Parent.Log(ScriptName, userName + ' not enough points')
-            return
-        # check correct target window
-        Parent.Log(ScriptName, 'getting active window')
-        activeWindow = getActiveWindow()
-        if settings['programName'].lower() not in activeWindow.lower() or settings['programName'].lower() == '':
-            if settings["debugMode"]:
-                Parent.Log(ScriptName, activeWindow + ' not the target window')
-            return
-        # perform action
-        action = randomHotkey()
-        kb.send(action[0])
-
-        output = settings["outputMsg"]
-        output = output.replace('$user', userName)
-        output = output.replace('$action', action[1])
-
-        if settings["debugMode"]:
-            Parent.Log(ScriptName, 'activating - ' + output)
-
-        if settings['useCost']:
-            Parent.RemovePoints(userID, userName, cost)
-
-        Parent.SendStreamMessage(output)
-        Parent.AddCooldown(ScriptName,settings['command'],settings['globalCd'])
-        Parent.AddUserCooldown(ScriptName,settings['command'],userID,settings['userCd'])
     return
 
 def getActiveWindow():
@@ -154,3 +100,138 @@ def Tick():
     :return: None
     """
     return
+
+
+def Unload():
+    """
+    Unload scripts and services, calls stop event
+    :return: None
+    """
+    StopEventReceiver()
+    return
+
+
+def ScriptToggled(state):
+    """
+    Event when script toggled ON/OFF
+    :param state: Boolean
+    :return: None
+    """
+    if state:
+        if EventReceiver is None:
+            RestartEventReceiver()
+    else:
+        StopEventReceiver()
+
+    return
+
+
+def StartEventReceiver():
+    """
+    Starts twitch Convert event receiver
+    :return: None
+    """
+    if settings["debugMode"]:
+        Parent.Log(ScriptName, "Starting receiver")
+
+    global EventReceiver
+    EventReceiver = TwitchPubSub()
+
+    EventReceiver.OnPubSubServiceConnected += EventReceiverConnected
+    EventReceiver.OnRewardRedeemed += EventReceiverRewardRedeemed
+
+    try:
+        EventReceiver.Connect()
+        if settings["debugMode"]:
+            Parent.Log(ScriptName, "connected")
+    except Exception as e:
+        Parent.Log(ScriptName, "Unable to start event receiver. Exception: " + str(e))
+    return
+
+
+def StopEventReceiver():
+    """
+    Stop twitch pubsub event receiver
+    :return: None
+    """
+    global EventReceiver
+    try:
+        if EventReceiver is None:
+            return
+        EventReceiver.Disconnect()
+        if settings["debugMode"]:
+            Parent.Log(ScriptName, "Event receiver disconnected")
+        EventReceiver = None
+
+    except Exception as e:
+        Parent.Log(ScriptName, "Event receiver already disconnected. Exception: " + str(e))
+    return
+
+
+def RestartEventReceiver():
+    """
+    Restarts receiver
+    :return: None
+    """
+    StopEventReceiver()
+    StartEventReceiver()
+    return
+
+
+def EventReceiverConnected(sender, e):
+    """
+    Twitch pubsub event callback for on connected event
+    Sets Reward topic listener
+    :param sender: /
+    :param e: /
+    :return: None
+    """
+    # create request payload
+    oauth = AnkhBotR2.Managers.GlobalManager.Instance.VMLocator.StreamerLogin.Token.replace("oauth:", "")
+    data = json.loads(Parent.GetRequest("https://id.twitch.tv/oauth2/validate", {"Authorization": 'OAuth ' + oauth}))
+    
+    if settings["debugMode"]:
+        Parent.Log(ScriptName, str(data))
+
+    channelId = json.loads(data["response"])['user_id']
+    if settings["debugMode"]:
+        Parent.Log(ScriptName, "Event receiver connected, sending topics for channel id: " + str(channelId))
+    
+    # Set pubsub to listen
+    EventReceiver.ListenToRewards(channelId)
+    EventReceiver.SendTopics(oauth)
+    return
+
+
+def EventReceiverRewardRedeemed(sender, e):
+    """
+    Processes rewards redeemed
+    :param sender: /
+    :param e: Claimed reward event info
+    :return: None
+    """
+    # if reward NOT assigned, stop
+    if e.RewardTitle.lower() != settings["rewardName"].lower():
+        return
+
+    # check correct target window
+    if settings["debugMode"]:
+        Parent.Log(ScriptName, 'getting active window')
+    activeWindow = getActiveWindow()
+    if settings['programName'].lower() not in activeWindow.lower() or settings['programName'].lower() == '':
+        if settings["debugMode"]:
+            Parent.Log(ScriptName, activeWindow + ' not the target window')
+        return
+
+    action = randomHotkey()
+    kb.send(action[0])
+
+    userName = e.DisplayName
+    output = settings["outputMsg"]
+    output = output.replace('$user', userName)
+    output = output.replace('$action', action[1])
+
+    if settings["debugMode"]:
+        Parent.Log(ScriptName, 'activating - ' + output)
+
+    Parent.SendStreamMessage(output)
